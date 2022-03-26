94e670faf01ab5ffa04033e24c5644cc4fa96d0f
AniP-C <68547683+AniP-C@users.noreply.github.com>
2022-03-26 22:09:45 +0530

    Add files via upload

---
+++ #Day1/Band_Name_Generator.py
@@ -0,0 +1,8 @@
+from unicodedata import name
+
+
+print(" Hello Welcome all of you to band name generator!")
+name= input("Enter Your city name")
+print("name")
+pet = input(" Enter your pet name")
+print(" Your best possible band name is " + name + " " + pet)
